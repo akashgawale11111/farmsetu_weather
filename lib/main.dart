24feb61c9c484f3224faf633ec78bc69f5675e03
akashgawale11111 <akashgawale027@gmail.com>
2026-03-21 17:33:46 +0530

    add

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 void main() async{
-    WidgetsFlutterBinding.ensureInitialized();
- // await Firebase.initializeApp(
-   // options: DefaultFirebaseOptions.currentPlatform,
-  //);
   runApp(
     const ProviderScope(
       child: MyApp(),
