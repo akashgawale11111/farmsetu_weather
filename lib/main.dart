94c59ca95862ce63caf3e952689c278c4869babb
akashgawale11111 <akashgawale027@gmail.com>
2026-03-22 15:27:37 +0530

    name

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FarmSetu Weather',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
