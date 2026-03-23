25bf6e83d863c852df0b38068ef46fe7c54059b6
akashgawale11111 <akashgawale027@gmail.com>
2026-03-23 18:49:43 +0530

    apis

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,16 +1,8 @@
-// class Constants {
-//   static const String baseUrl = 'api.openweathermap.org/data/2.5';
-//   static const String weatherBaseUrl = 'https://api.open-meteo.com/v1/forecast';
-//   static const String geocodingBaseUrl = 'https://nominatim.openstreetmap.org';
-//   static const String apiKey = 'ec4562fa52f6306b4d588c08a0848b39'; // Replace with your OpenWeatherMap API key
-//   static const String mapApiKey = 'AIzaSyBGv9znbx4hAdCp_6YK0-HO2XVKI4ZXALk'; // Same as OpenWeather key
-// }
 
 class Constants {
   static const String openWeatherApiKey = 'ec4562fa52f6306b4d588c08a0848b39'; // Replace with your key
   static const String weatherBaseUrl = 'https://api.openweathermap.org/data/2.5';
   static const String geocodingBaseUrl = 'https://api.openweathermap.org/geo/1.0';
-   static const String openAIApiKey = 'your-openai-api-key';
   static const String openAIBaseUrl = 'https://api.openai.com/v1/chat/completions';
    static const String apiKey = "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";
   static const String baseUrl = "https://api.openai.com/v1/chat/completions";
